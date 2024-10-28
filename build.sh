7afbd326a1ee53e0a48e9b85e9d358cd81a1395d
Bracr10 <Brianramirez01arias@gmail.com>
2024-10-27 22:20:45 -0600

    Updates
    New codes linking C+ASM

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gcc -o byteSize.out byteSize.c 
+./byteSize.out
+
+
+nasm -f elf32 hll_test.asm 
+gcc -m32 -o test1.out hll_ex1c.c hll_test.o 
+./test1.out
+
+nasm -f elf32 hll_minmaxa.asm
+gcc -m32 -o minmax.out hll_minmaxc.c hll_minmaxa.o 
+./minmax.out
+
+
---
+++ byteSize.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main() {
+    printf("\nBienvenido a la calculadora de tamaño de tipos de datos primitivo en C \n\n");
+    //Datos primitivos
+    printf("Tamaño de un caracter char %ld byte\n", sizeof(char));
+    printf("Tamaño de un entero paqueño short int %ld byte\n", sizeof(short));
+    printf("Tamaño de un entero grande long %ld byte\n", sizeof(long int));
+    printf("Tamaño de un entero int %ld byte\n", sizeof(int));
+    printf("Tamaño de un flotante float %ld byte\n", sizeof(float));
+    printf("Tamaño de un doble double %ld byte\n", sizeof(double));
+    printf("Tamaño de un doble largo long double %ld byte\n", sizeof(long double));
+    
+    printf("\n\nRangos de tipos de datos en C:\n\n");
+
+    // Tipo char
+    printf("char:\n");
+    printf("  Con signo: %d a %d\n", CHAR_MIN, CHAR_MAX);
+    printf("  Sin signo: 0 a %u\n", UCHAR_MAX);
+
+    // Tipo short
+    printf("short:\n");
+    printf("  Con signo: %d a %d\n", SHRT_MIN, SHRT_MAX);
+    printf("  Sin signo: 0 a %u\n", USHRT_MAX);
+
+    // Tipo int
+    printf("int:\n");
+    printf("  Con signo: %d a %d\n", INT_MIN, INT_MAX);
+    printf("  Sin signo: 0 a %u\n", UINT_MAX);
+
+    // Tipo long
+    printf("long:\n");
+    printf("  Con signo: %ld a %ld\n", LONG_MIN, LONG_MAX);
+    printf("  Sin signo: 0 a %lu\n", ULONG_MAX);
+
+
+    // Tipos de punto flotante
+    printf("float:\n");
+    printf("  Con signo: %.3e a %.3e\n", FLT_MIN, FLT_MAX);
+    printf("  Sin signo:  0 a %.3e \n", FLT_MAX);
+
+    printf("double:\n");
+    printf("  Con signo: %.3e a %.3e\n", DBL_MIN, DBL_MAX);
+    printf("  Sin signo:  0 a %.3e \n", DBL_MAX);
+
+    printf("long double:\n");
+    printf("  Con signo: %.3Le a %.3Le\n", LDBL_MIN, LDBL_MAX);
+    printf("  Sin signo:  0 a %.3Le \n", LDBL_MAX);
+}
